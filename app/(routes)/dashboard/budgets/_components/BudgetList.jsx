2cabb6db14194e46d6b3e90dfa8ae1913e9dda41
Happy Felix Chukwuma <happychukwuma@gmail.com>
2024-06-27 20:32:11 +0100

    Created the Budget page view

---
+++ app/(routes)/dashboard/budgets/_components/BudgetList.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import CreateBudget from './CreateBudget'
+
+function BudgetList() {
+  return (
+    <div className='mt-7'><CreateBudget/></div>
+  )
+}
+
+export default BudgetList
